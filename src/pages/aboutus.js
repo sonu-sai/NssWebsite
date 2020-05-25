7dcb4ce31b8f69cf9797e7fb1fe71aa528a97b69
karnan <karnan@pop-os.localdomain>
2020-05-25 20:16:02 +0530

    aboutus cntent

--- src/pages/aboutus.js
+++ src/pages/aboutus.js
@@ -8,7 +8,45 @@ const AboutUsPage = () => (
     <SEO title="AboutUs" />
     <Header />
     <div className="container">
-      <h1>This is About Us Page</h1>
+      <div className="mainText">
+        <h1>ABOUT US</h1>
+        NSS NSSCE, the student volunteer body under the jurisdiction of Ministry
+        of youth affairs and sports is the largest student volunteer body in the
+        campus of NSS College of Engineering, Palakkad. We are a joint student
+        volunteer body of two NSS units, 128&198 and is a part of Technical cell
+        of National Service Scheme. NSS NSSCE was established in the college
+        campus half a centum ago on 1969 with the aim of encouraging student
+        participation in public welfare activities using their knowledge and
+        skill. Throughout these five decades, NSS NSSCE has grown to be one of
+        the best student volunteer body in the state. We are a group of 200
+        volunteers who wish to bring a change or a hope however small it maybe.
+        Motivated by the principle of NOT ME BUT YOU, we try to organise as many
+        activities and give support to various public programs that can bring
+        about a small change in person life or can bring a small benefit for the
+        society. Many of our activities have been publicly recognised and
+        appreciated important ones being Punarjani, Pratheeksha, Santhwanam and
+        Unarv. Punarjani is a regeneration programme aimed to regenerate public
+        assets. As a part of this programme, we were able to regenerate public
+        assets worth crores. Pratheeksha is our blood donation unit. As a part
+        of Pratheeksha, we organise blood donation camps every year and we also
+        arrange blood for emergency cases in all places of our reach. Santhvanam
+        is our fund-raising program aimed at providing financial assistance to
+        the needy. Unarv is an educational empowerment programme aiming for the
+        empowerment of students from rural areas and to help them break their
+        shell of inferior feelings as well as to give them the exposure to the
+        outside world and world of opportunities. Apart from these main
+        programmes we also do many other activities including LED bulb making,
+        paper pen making, campus cleaning, anti-drug programmes, and LED-Green.
+        We provide assistance to the district administrations by conducting
+        various surveys and data collection. NSS NSSCE is always at the front in
+        various public, social and environmental welfare programmes living upto
+        the expectation of one of the best student volunteer body. We are a
+        group of youths who wish to make the best part of our life worthy, to be
+        a light for those who are in the dark, to be a rope for those who are in
+        the pit, to be a shoulder for the week to rely on and to be the helping
+        hand at the right time. This is what we wish for, this is what we aim to
+        do.
+      </div>
     </div>
     <Footer />
   </div>
